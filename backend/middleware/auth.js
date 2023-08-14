f86cdec03aad03254dad8b35aed7f8aa6901d561
julef95 <lefevreju@hotmail.fr>
2023-08-14 16:00:45 +0200

    ajout grain de sel dans fichier .env

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,13 @@
+// Charge les variables d'environnement dans le fichier .env
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
  
 module.exports = (req, res, next) => {
    try {
        const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, 'TOKEN');
+       // Importe le grain de sel contenu dans le fichier .env
+       const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
        const userId = decodedToken.userId;
        req.auth = {
            userId: userId
